e4cafe7b01a60a81bdcc12071b7af63322e5fe10
DANIEL <danigomo0709@gmail.com>
2023-10-09 11:42:04 -0600

    fiufiu

---
+++ src/Chicha/Chicha.java
@@ -0,0 +1,10 @@
+package Chicha;
+
+public class Chicha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/RGB/RGB.java
@@ -0,0 +1,43 @@
+package RGB;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class RGB {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RGB window = new RGB();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public RGB() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
